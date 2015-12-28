048845c2ddf27830b9a709ce902fd9300607d3a7
ivan miranda <ivan@pengostores.com>
2015-12-28 17:44:19 -0600

    Soporte de multiples BD

--- Sfphp/BaseDatos.php
+++ Sfphp/BaseDatos.php
@@ -30,77 +30,72 @@
 # -----------------------
 # Manejador principal de la base de datos
 # -----------------------
-
 final class Sfphp_BaseDatos {  
-    static private $default; 
+    static private $origen;
+    static private $conexion;
     static private $instancia;
    #Conexion a la BD desde la creación de la clase
     private function __construct($configuracion = "default") {
         $base = Sfphp_Config::get('bases');
         $base = $base[$configuracion];
-        if(!self::$default) {
-            try {
-                if(!isset($base["charset"]))
-                    $base["charset"] = "utf8";
+        self::$origen = $base["type"];
+        try {
+            if(!isset($base["charset"]))
+                $base["charset"] = "utf8";
+            $parametros = array();
+            if($base["type"] == "mysql")
+                $parametros = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES '. $base["charset"]);
+            else
                 $parametros = array();
-                if($base["type"] == "mysql")
-                    $parametros = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES '. $base["charset"]);
-                else
-                    $parametros = array();
-                switch ($base["type"]) {
-                    case 'sqlsrv':
-                        self::$default = new PDO($base["type"].":Server=".$base["host"].";",
-                            $base["user"], Sfphp::decrypt($base["password"]), $parametros);
-                        break;
-                    case 'mysql':
-                        self::$default = new PDO($base["type"].":host=".$base["host"].";dbname=".$base["dbname"],
-                            $base["user"], Sfphp::decrypt($base["password"]), $parametros);
-                        break;
-                    case 'firebird':
-                        $parametros = array(
-                            PDO::FB_ATTR_TIMESTAMP_FORMAT,"%d-%m-%Y",
-                            PDO::FB_ATTR_DATE_FORMAT ,"%d-%m-%Y"
-                        );
-                        self::$default = new PDO($base["type"].":dbname=".$base["host"].$base["dbname"], $base["user"], Sfphp::decrypt($base["password"]), $parametros);
-                        break;
-                    default:
-                        self::$default = new PDO($base["type"].":host=".$base["host"].";dbname=".$base["dbname"],
-                            $base["user"], Sfphp::decrypt($base["password"]));
-                        break;
-                }
-            } catch (PDOException $e) { 
-                Sfphp_Logs::procesa($e);
+            switch ($base["type"]) {
+                case 'sqlsrv':
+                    self::$conexion = new PDO($base["type"].":Server=".$base["host"].";",
+                        $base["user"], Sfphp::decrypt($base["password"]), $parametros);
+                    break;
+                case 'mysql':
+                    self::$conexion = new PDO($base["type"].":host=".$base["host"].";dbname=".$base["dbname"],
+                        $base["user"], Sfphp::decrypt($base["password"]), $parametros);
+                    break;
+                case 'firebird':
+                    $parametros = array(
+                        PDO::FB_ATTR_TIMESTAMP_FORMAT,"%d-%m-%Y",
+                        PDO::FB_ATTR_DATE_FORMAT ,"%d-%m-%Y"
+                    );
+                    self::$conexion = new PDO($base["type"].":dbname=".$base["host"].$base["dbname"], $base["user"], Sfphp::decrypt($base["password"]), $parametros);
+                    break;
+                default:
+                    self::$conexion = new PDO($base["type"].":host=".$base["host"].";dbname=".$base["dbname"],
+                        $base["user"], Sfphp::decrypt($base["password"]));
+                    break;
             }
+        } catch (PDOException $e) { 
+            Sfphp_Logs::procesa($e);
         }
-        //return self::$default;              
     }
-
     public static function get($base = "default")
     {
-        if(!self::$instancia instanceof self)
+    # Conectar a la base, sólo si es distinta a la actual
+        if($base != self::$origen)
             self::$instancia = new self($base);
         return self::$instancia;
     }
-
     public function beginTransaction() {
-        return self::$default->beginTransaction();
+        return self::$conexion->beginTransaction();
     }
-
     public function commit() {
-        return self::$default->commit();
+        return self::$conexion->commit();
     }
-
     public function errorCode() {
-        return self::$default->errorCode();
+        return self::$conexion->errorCode();
     }
     
     public function errorInfo() {
-        return self::$default->errorInfo();
+        return self::$conexion->errorInfo();
     }
    #Ejecucion de querys, con soporte para pase de parametros en un arreglo
     public function query($consulta, $valores = array()) {
         $resultado = false;
-        if($statement = self::$default->prepare($consulta)) {
+        if($statement = self::$conexion->prepare($consulta)) {
             if(preg_match_all("/(:\w+)/", $consulta, $campo, PREG_PATTERN_ORDER)) {
                 $campo = array_pop($campo);
                 foreach($campo as $parametro){
@@ -123,7 +118,7 @@ final class Sfphp_BaseDatos {
     #Ejecucion de querys, para uso en el propio framework (construccion de modelos automaticos)
     public function raw_query($consulta, $valores = array()) {
         $resultado = false;
-        if($statement = self::$default->prepare($consulta)) {
+        if($statement = self::$conexion->prepare($consulta)) {
             if(preg_match_all("/(:\w+)/", $consulta, $campo, PREG_PATTERN_ORDER)) {
                 $campo = array_pop($campo);
                 foreach($campo as $parametro) {
@@ -148,7 +143,7 @@ final class Sfphp_BaseDatos {
     #Ejecucion de INSERT
     public function insert($consulta, $valores = array()) {
         $resultado = false;
-        if($statement = self::$default->prepare($consulta)) {
+        if($statement = self::$conexion->prepare($consulta)) {
             if(preg_match_all("/(:\w+)/", $consulta, $campo, PREG_PATTERN_ORDER)) {
                 $campo = array_pop($campo);
                 foreach($campo as $parametro){
@@ -158,7 +153,7 @@ final class Sfphp_BaseDatos {
             try {
                 if (!$statement->execute())
                     throw new PDOException("[SQLSTATE] ".$statement->errorInfo()[2],$statement->errorInfo()[1]);
-                $resultado = self::$default->lastInsertId();
+                $resultado = self::$conexion->lastInsertId();
             }
             catch(PDOException $e) {
                 Sfphp_Logs::procesa($e);
