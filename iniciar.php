7514568288590ba79d193be9ebe7cb014a832ef2
ivan miranda <ivan@pengostores.com>
2016-01-19 13:18:55 -0600

    Script de inicializacion

--- iniciar.php
+++ iniciar.php
@@ -32,8 +32,23 @@
 # y un archivo de configuración con los parametros obligados
 # -----------------------
 
-require_once './Sfphp/_base.php';
-
+require_once './Sfphp/__base.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo APP_NAME ?></title>
+     <!-- Bootstrap Table -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <link rel="shortcut icon" href="{BASE_URL}favicon.ico">
+</head>
+<body>
+    <div class="panel panel-primary">
+        <div class="panel-heading">Simple Framework (for) PHP</div>
+<?php 
 if(!file_exists("./Etc/Config/config.xml")) {
 	echo "Inicializando el framework...<br>";
 	file_put_contents("./sfphp.md5", Sfphp_Disco::MD5("./Sfphp"));
@@ -43,18 +58,18 @@ if(!file_exists("./Etc/Config/config.xml")) {
 		mkdir("./App/Core", 0770);
 		mkdir("./App/Core/Controladores", 0770);
 		mkdir("./App/Core/Modelos", 0770);
-		mkdir("./App/Core/Vistas", 0770);
+		mkdir("./App/Vistas", 0770);
 		file_put_contents("./App/.htaccess", "Options -Indexes");
 		file_put_contents("./App/Core/.htaccess", "Options -Indexes");
 		file_put_contents("./App/Core/Controladores/.htaccess", "Options -Indexes");
 		file_put_contents("./App/Core/Modelos/.htaccess", "Options -Indexes");
-		file_put_contents("./App/Core/Vistas/.htaccess", "Options -Indexes");
+		file_put_contents("./App/Vistas/.htaccess", "Options -Indexes");
 		file_put_contents("./App/Core/Controladores/Inicio.php", "<?php class Controladores_Inicio extends Sfphp_Controlador{ public function inicio() { echo 'sfphp2::Hola Mundo'; } }");
 		chmod("./App/.htaccess", 0750);
 		chmod("./App/Core/.htaccess", 0750);
 		chmod("./App/Core/Controladores/.htaccess", 0750);
 		chmod("./App/Core/Modelos/.htaccess", 0750);
-		chmod("./App/Core/Vistas/.htaccess", 0750);
+		chmod("./App/Vistas/.htaccess", 0750);
 		chmod("./App/Core/Controladores/Inicio.php", 0770);
 	}
 	if(!is_dir("./Libs"))
@@ -63,14 +78,17 @@ if(!file_exists("./Etc/Config/config.xml")) {
 		mkdir("./Etc", 0750);
 		mkdir("./Etc/Config", 0750);
 		mkdir("./Etc/Logs", 0750);
+		mkdir("./Etc/Cache", 0750);
 		mkdir("./Etc/Sesiones", 0750);
 		file_put_contents("./Etc/.htaccess", "Options -Indexes");
 		file_put_contents("./Etc/Config/.htaccess", "Options -Indexes");
 		file_put_contents("./Etc/Logs/.htaccess", "Options -Indexes");
+		file_put_contents("./Etc/Cache/.htaccess", "Options -Indexes");
 		file_put_contents("./Etc/Sesiones/.htaccess", "Options -Indexes");
 		chmod("./Etc/.htaccess", 0750);
 		chmod("./Etc/Config/.htaccess", 0750);
 		chmod("./Etc/Logs/.htaccess", 0750);
+		chmod("./Etc/Cache/.htaccess", 0750);
 		chmod("./Etc/Sesiones/.htaccess", 0750);
 	}
 	echo "Inicializando archivo de configuración...<br>";
@@ -80,6 +98,7 @@ if(!file_exists("./Etc/Config/config.xml")) {
 			'key' => $_llave_encripcion,
 			'name' => 'sfphp',
 			'company' => 'sincco.com',
+			'cache' => '600',
 		),
 		'front' => array (
 			'url' => sprintf("%s://%s%s",
@@ -89,33 +108,28 @@ if(!file_exists("./Etc/Config/config.xml")) {
 			),
 		),
 		'bases' => array (
-			'base' => array(
-				'host' => 'sfphp',
+			'default' => array(
+				'host' => 'localhost',
 				'user' => 'sfphp',
 				'password' => Sfphp::encrypt('sfphp',$_llave_encripcion),
 				'dbname' => 'sfphp',
 				'type' => 'mysql',
 				'default' => 1,
 			),
-			'type' => "DEFAULT",
-			'name' => "sfphp",
-			'ssonly' => 0,
-			'inactivity' => 60,
 		),
 		'session' => array (
 			'type' => "DEFAULT",
 			'name' => "sfphp",
 			'ssonly' => 0,
-			'inactivity' => 60,
+			'inactivity' => 300,
 		),
 		'dev' => array (
 			'log' => 1,
-			'showerrors' => 0,
-			'showphperrors' => 0,
+			'showerrors' => 1,
+			'querylog' => 0,
 		),
 	);
-	if(Sfphp_Disco::grabaXML($_config,"config","./Etc/Config/config.xml"))
-	{
+	if(Sfphp_Disco::grabaXML($_config,"config","./Etc/Config/config.xml")) {
 		chmod("./Etc/Config/config.xml", 0770);
 		echo "Configuración basica completa.<br>";
 		echo "<a href=\"./\">Ve al inicio de tu app</a>";
@@ -126,3 +140,12 @@ if(!file_exists("./Etc/Config/config.xml")) {
 	echo "El framework ya esta configurado<br>";
 	echo "<a href=\"./\">Ve al inicio de tu app</a>";
 }
+?>
+    </div>
+    <nav>
+        <ul class="pager">
+            <li class="previous"><a href="javascript:window.history.back()"><span aria-hidden="true">&larr;</span> Regresar</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
